5807c5991c13b78295b1068300ad950953cb5192
wut <talonpopy@gmail.com>
2026-02-03 18:22:33 +0700

    fix merge

--- Main.c
+++ Main.c
@@ -24,15 +24,9 @@ int calculate(int a, char op, int b) {
     int total = a;
 
 //    if (op == '+') total = plus(a, b);
-<<<<<<< HEAD
     else if (op == '-') total = minus(a, b);
 //    else if (op == '*') total = multiply(a, b);
    else if (op == '/') total = divide(a, b);
-=======
-//    else if (op == '-') total = minus(a, b);
-      else if (op == '*') total = multiply(a, b);
-//    else if (op == '/') total = divide(a, b);
->>>>>>> multiply
 //    else if (op == '%') total = Persen(a, b);
 
     printf("Process: %d %c %d = %d\n", a, op, b, total);
