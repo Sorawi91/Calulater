e86000ce815cea6a76b573e03924ac37c7699f28
Guitar <5178gt@gmail.com>
2026-02-03 17:33:12 +0700

    add minus function & condition

--- Main.c
+++ Main.c
@@ -12,7 +12,7 @@
     return a * b;
 }
 
-//int minus(int a, int b) {
+int minus(int a, int b) {
     return a - b;
 }
 
@@ -24,7 +24,7 @@ int calculate(int a, char op, int b) {
     int total = a;
 
 //    if (op == '+') total = plus(a, b);
-//    else if (op == '-') total = minus(a, b);
+    else if (op == '-') total = minus(a, b);
 //    else if (op == '*') total = multiply(a, b);
 //    else if (op == '/') total = divide(a, b);
 //    else if (op == '%') total = Persen(a, b);
